887ed50f48b2fbb00e7d1d2f6c5f71f7142412f6
KBEUM <ligit992@gmail.com>
2020-11-27 20:16:42 +0900

    Updates

---
+++ precache-manifest.29f3dc54b3b8496d1f547ba8ab0a3d59.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694c8fd6490778729c08c3a375edb791",
+    "url": "/bomflix/index.html"
+  },
+  {
+    "revision": "e585225062068a541879",
+    "url": "/bomflix/static/css/2.b8329af8.chunk.css"
+  },
+  {
+    "revision": "72c50dbd715ff2cbce42",
+    "url": "/bomflix/static/css/main.dcdcdd93.chunk.css"
+  },
+  {
+    "revision": "e585225062068a541879",
+    "url": "/bomflix/static/js/2.dbe8450f.chunk.js"
+  },
+  {
+    "revision": "bd5dd8782d3c3a48bd225c1d250ef019",
+    "url": "/bomflix/static/js/2.dbe8450f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c50dbd715ff2cbce42",
+    "url": "/bomflix/static/js/main.549b8d69.chunk.js"
+  },
+  {
+    "revision": "88e323b3f9d307939565",
+    "url": "/bomflix/static/js/runtime-main.e1af730f.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/bomflix/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/bomflix/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/bomflix/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/bomflix/static/media/slick.f97e3bbf.svg"
+  }
+]);
